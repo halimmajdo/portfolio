ebd9610e725acb4d47249ab1924cbcda3b00dc40
Abdul Halim Majdo <halimmajdo@gmail.com>
2026-06-28 20:31:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple console log confirmation when someone loads your portfolio
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("System initialized: Portfolio successfully loaded.");
+});
